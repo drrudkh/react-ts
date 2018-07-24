bf26452b89327ff6bd3179996cf1b30318858fae
tudor.apostol@hogarthww <tudor.apostol@hogarthww.com>
2018-07-24 17:10:50 +0300

    new enhancer constant

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,10 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 
-// APP COMPONENT
 import App from "./app/components/App";
-
-// REDUCERS
 import shoppingCartReducer from "./app/store/reducers/shoppingCart";
 import productsReducer from './app/store/reducers/products';
-
-// MIDDLEWARE
 import logger from './app/store/middleware/logger';
-
-// SASS
 import "./scss/main.scss";
 
 const ROOT = document.getElementById("app");
@@ -21,11 +14,10 @@ const ROOT_REDUCER = combineReducers({
   cart: shoppingCartReducer,
   products: productsReducer
 })
-
-// REDUX DEVTOOLS
 const COMPOSE_ENHANCERS = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const ENHANCER = COMPOSE_ENHANCERS(applyMiddleware(logger));
 
-const STORE = createStore(ROOT_REDUCER, COMPOSE_ENHANCERS(applyMiddleware(logger)));
+const STORE = createStore(ROOT_REDUCER, ENHANCER);
 
 ReactDOM.render(
   <Provider store={STORE}>
